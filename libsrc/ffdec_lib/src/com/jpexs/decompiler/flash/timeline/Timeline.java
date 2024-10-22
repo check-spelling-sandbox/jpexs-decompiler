49abca09df9bb79ffd8f376775d48660eb0cbb2b
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-10-22 09:56:34 -0400

    spelling: populate
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/timeline/Timeline.java
+++ libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/timeline/Timeline.java
@@ -638,7 +638,7 @@ public class Timeline {
 
         createASPackages();
         if (timelined instanceof SWF) {
-            // popuplate only for main timeline
+            // populate only for main timeline
             populateSoundStreamBlocks(-1, timelined.getTags());
         }
 
