755bb9f7148d4b9b52c01f1b83d2c8a5caac0f76
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-10-22 09:53:35 -0400

    spelling: parenthesis
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/action/model/FSCommandActionItem.java
+++ libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/action/model/FSCommandActionItem.java
@@ -65,7 +65,7 @@ public class FSCommandActionItem extends ActionItem {
     @Override
     public GraphTextWriter appendTo(GraphTextWriter writer, LocalData localData) throws InterruptedException {
         writer.append("fscommand");
-        writer.spaceBeforeCallParenthesies(1);
+        writer.spaceBeforeCallParenthesis(1);
         writer.append("(");
         command.appendTry(writer, localData);
         if (parameter != null) {
--- libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/action/model/LoadMovieNumActionItem.java
+++ libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/action/model/LoadMovieNumActionItem.java
@@ -86,7 +86,7 @@ public class LoadMovieNumActionItem extends ActionItem {
             methodStr = ",\"POST\"";
         }
         writer.append("loadMovieNum");
-        writer.spaceBeforeCallParenthesies(2);
+        writer.spaceBeforeCallParenthesis(2);
         writer.append("(");
         urlString.toString(writer, localData);
         writer.append(",");
