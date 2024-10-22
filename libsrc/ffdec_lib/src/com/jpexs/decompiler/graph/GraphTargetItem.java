152611cc12a583340d69ce828fe034b6e8b0305c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-10-22 08:38:47 -0400

    spelling: beginning
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- libsrc/ffdec_lib/src/com/jpexs/decompiler/graph/GraphTargetItem.java
+++ libsrc/ffdec_lib/src/com/jpexs/decompiler/graph/GraphTargetItem.java
@@ -1062,7 +1062,7 @@ public abstract class GraphTargetItem implements Serializable, Cloneable {
      */
     public GraphTextWriter appendCommands(GraphTargetItem prevLineItem, GraphTextWriter writer, LocalData localData, List<GraphTargetItem> commands, boolean asBlock) throws InterruptedException {
 
-        //This may be useful in the future, but we must handle obfuscated SWFs where there is only one debugline instruction on the beggining.
+        //This may be useful in the future, but we must handle obfuscated SWFs where there is only one debugline instruction on the beginning.
         final boolean useLineInfo = false;
 
         int prevLine = prevLineItem == null ? 0 : prevLineItem.getLine();
